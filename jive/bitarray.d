5befee2dd0756cd99e04de7c84c1aad3768f29b1
Simon Bürger <simon.buerger@rwth-aachen.de>
2016-03-19 16:01:40 +0100

    restructure BitArray to be resizable

--- jive/bitarray.d
+++ jive/bitarray.d
@@ -7,38 +7,19 @@ private import core.bitop;
  */
 struct BitArray
 {
+	//////////////////////////////////////////////////////////////////////
+	/// internals
+	//////////////////////////////////////////////////////////////////////
+
 	alias limb = size_t;
 	enum limbBits = limb.sizeof*8;
 
-	private union
-	{
-		limb* buf = null;
-		limb small;
-	}
-	private size_t size = 0;	// number of bits in use
+	private limb[] buf = null;	// unused bits are always 0
+	private size_t size = 0;	// number of bits actually in use
 
-	inout(limb)* ptr() inout pure nothrow @property
+	inout(limb)* ptr() inout pure nothrow @property @safe
 	{
-		if(size <= limbBits)
-			return &small;
-		else
-			return buf;
-	}
-
-	/** constructor for given length */
-	this(size_t size) pure
-	{
-		this.size = size;
-
-		if(size > limbBits)
-			buf = new limb[(size+limbBits-1)/limbBits].ptr;
-	}
-
-	/** post-blit that does a full copy */
-	this(this)
-	{
-		if(size > limbBits)
-			buf = buf[0..(size+limbBits-1)/limbBits].dup.ptr;
+		return buf.ptr;
 	}
 
 	/** number of elements */
@@ -47,11 +28,75 @@ struct BitArray
 		return size;
 	}
 
-	/** number of limbs */
+	/** number of limbs in use */
 	size_t limbCount() const pure nothrow @property @safe
 	{
-		return (size+limbBits-1)/limbBits;
+		return (length + limbBits - 1) / limbBits;
 	}
+
+	/** returns limbs in use */
+	inout(limb)[] limbs() inout pure nothrow @property
+	{
+		return ptr[0..limbCount];
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	/// constructors and whole-array operations
+	//////////////////////////////////////////////////////////////////////
+
+	/** constructor for given length */
+	this(size_t size) pure
+	{
+		this.size = size;
+		buf = new limb[limbCount];
+	}
+
+	/** post-blit that does a full copy */
+	this(this)
+	{
+		buf = limbs.dup;
+	}
+
+	void reserve(size_t cap)
+	{
+		if(cap <= limbBits * buf.length)
+			return;
+
+		buf.length = (cap + limbBits - 1) / limbBits;
+	}
+
+	/** either cuts of or fills new elements with false */
+	void resize(size_t newSize)
+	{
+		reserve(newSize);
+
+		for(size_t i = newSize; i < size; ++i)
+			this[i] = false;
+		size = newSize;
+	}
+
+	/** count number of elements equal to v */
+	size_t count(bool v) const pure nothrow
+	{
+		// NOTE: relies on unused bits being zero
+		size_t c;
+		foreach(x; (cast(uint*)ptr)[0..(size+31)/32])
+			c += popcnt(x); // why is there only a 32 bit version of popcnt in core.bitop?
+		if(v)
+			return c;
+		else
+			return size-c;
+	}
+
+	/** set all elements to false */
+	void reset() pure nothrow
+	{
+		limbs[] = 0;
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	/// indexing and iteration
+	//////////////////////////////////////////////////////////////////////
 
 	/** indexing */
 	bool opIndex(size_t i) const pure nothrow
@@ -68,18 +113,6 @@ struct BitArray
 			bts(ptr, i);
 		else
 			btr(ptr, i);
-	}
-
-	size_t count(bool v) const pure nothrow
-	{
-		// NOTE: relies on unused bits being zero
-		size_t c;
-		foreach(x; (cast(uint*)ptr)[0..(size+31)/32])
-			c += popcnt(x); // why is there only a 32 bit version of popcnt in core.bitop?
-		if(v)
-			return c;
-		else
-			return size-c;
 	}
 
 	/** set element i to true. returns false if it already was. */
