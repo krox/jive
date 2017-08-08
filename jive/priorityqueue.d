b0c5a6b2a5e8bd72c4f5d7c906d677e9ed2904aa
Simon Bürger <simon.buerger@rwth-aachen.de>
2017-08-08 16:53:54 +0200

    remove PriorityQueue with mutable entries
    Implementation was too specialized. Use PriorityArray instead.

--- jive/priorityqueue.d
+++ jive/priorityqueue.d
@@ -1,20 +1,20 @@
+/**
+License: public domain
+Authors: Simon Bürger
+*/
 module jive.priorityqueue;
 
-private import jive.internal;
-private import jive.array;
-private import std.algorithm;
-private import std.range;
-private import std.functional;
+import jive.internal;
+import jive.array;
+import std.algorithm;
+import std.range;
+import std.functional;
 
 /**
  * Priority queue which allows fast access to the smallest element.
  * Implemented as binary heap embedded into a jive.Array.
- * If mutable is true:
- *    - order of values inside queue may be changed (need to call increase/decrease/push accordingly)
- *    - V must be castable to (small, positive) int
- *    - values inside the queue are unique w.r.t. their int representation
  */
-struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
+struct PriorityQueue(V, alias _pred = "a < b")
 {
 	mixin PredicateHelper!(_pred, V);
 
@@ -24,6 +24,8 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 
 	static if(dynamicPred)
 	{
+		@disable this();
+
 		this(_pred p)
 		{
 			this.pred = p;
@@ -56,8 +58,6 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 		}
 	}
 
-	// Note: postblit/destructor is automatically generated
-
 
 	//////////////////////////////////////////////////////////////////////
 	// metrics
@@ -84,16 +84,12 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 	/** removes all elements but keeps allocated memory */
 	void clear()
 	{
-		arr.resize(0);
-		static if(mutable)
-			location.resize(0);
+		arr.clear();
 	}
 
 	/** clear the heap and return all elements (unordered!) */
 	Array!V release()
 	{
-		static if(mutable)
-			location.resize(0);
 		return move(arr);
 	}
 
@@ -108,52 +104,34 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 		return arr.front;
 	}
 
-	/** ditto */
-	alias front top;
-
 	/**
 	 * Remove the first element (i.e. the smallest) from the queue.
 	 * returns: the removed element
 	 */
-	V popFront()
+	V pop()
 	{
 		assert(!empty);
 		swap(arr[0], arr.back);
 		V r = arr.popBack;
-		static if(mutable)
-			location[cast(int)r] = -1;
 		if(!empty)
 			percolateDown(0);
 		return r;
 	}
 
 	/** ditto */
-	alias popFront pop;
+	alias popFront = pop;
 
 	/**
 	 * Add an element to the queue.
-	 * If modify is true and the element is already in, this is equivalent to update.
 	 */
-	void pushBack(V value)
+	void push(V value)
 	{
-		static if(mutable)
-		{
-			if(location.length <= cast(size_t)value)
-				location.resize(max(cast(size_t)value + 1, location.length*2), -1);
-			else if(location[cast(int)value] != -1)
-			{
-				percolateUp(location[cast(int)value]);
-				percolateDown(location[cast(int)value]);
-				return;
-			}
-		}
-
 		arr.pushBack(move(value));
 		percolateUp(cast(int)length-1);
 	}
 
 	/** Add multiple elements to the queue. */
-	void pushBack(Stuff)(Stuff data)
+	void push(Stuff)(Stuff data)
 		if(!is(Stuff:V) && isInputRange!Stuff && is(ElementType!Stuff:V))
 	{
 		static if(hasLength!Stuff)
@@ -163,39 +141,7 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 			pushBack(x);
 	}
 
-	static if(mutable)
-	{
-		/** check wether a value is currently present in th heap */
-		bool opIn_r(V value)
-		{
-			return cast(int)value < location.length && location[cast(int)value] != -1;
-		}
-
-		/** notify the heap that a value has increased in order */
-		void increase(V value)
-		{
-			assert(value in this);
-			percolateDown(location[cast(int)value]);
-		}
-
-		/** notify the heap that a value has decreased in order */
-		void decrease(V value)
-		{
-			assert(value in this);
-			percolateUp(location[cast(int)value]);
-		}
-
-		/** notify the heap that a value has changed in order */
-		void update(V value)
-		{
-			assert(value in this);
-			percolateUp(location[cast(int)value]);
-			percolateDown(location[cast(int)value]);
-		}
-	}
-
-	/** ditto */
-	alias pushBack push;
+	alias pushBack = push;
 
 
 	//////////////////////////////////////////////////////////////////////
@@ -203,8 +149,6 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 	//////////////////////////////////////////////////////////////////////
 
 	private Array!V arr;
-	static if(mutable)
-		private Array!int location; // -1 for stuff that is not in the heap currently
 
 	private int parent(int i) { return (i-1)/2; }
 	private int left(int i) { return 2*i+1; }
@@ -223,15 +167,9 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 		auto x  = move(arr[i]);
 
 		for(int p = parent(i); i != 0 && pred(x, arr[p]); i = p, p = parent(i))
-		{
 			arr[i] = move(arr[p]);
-			static if(mutable)
-				location[cast(int)arr[i]] = i;
-		}
 
 		arr[i] = move(x);
-		static if(mutable)
-			location[cast(int)x] = i;
 	}
 
 	private void percolateDown(int i)
@@ -239,33 +177,37 @@ struct PriorityQueue(V, alias _pred = "a < b", bool mutable = false)
 		auto x = move(arr[i]);
 
 		for(int c = smallerChild(i); c < length && pred(arr[c], x); i = c, c = smallerChild(i))
-		{
 			arr[i] = move(arr[c]);
-			static if(mutable)
-				location[cast(int)arr[i]] = i;
-		}
 
 		arr[i] = move(x);
-		static if(mutable)
-			location[cast(int)x] = i;
 	}
 }
 
 
 unittest
 {
+	// custom predicate turns a min-heap into a max-heap
+	PriorityQueue!(int, "a > b") q;
+	q.push([7,9,2,3,4,1,6,5,8,0]);
+
+	int i = 9;
+	while(!q.empty)
+		assert(q.pop == i--);
+}
+
+unittest
+{
+	// custom comparator that can contain state
+	struct Compare
 	{
-		auto q = PriorityQueue!(int, "a > b")([7,9,2,3,4,1,6,5,8,0]);
-		int i = 9;
-		while(!q.empty)
-			assert(q.pop == i--);
+		bool opCall(int a, int b)
+		{ return a < b; }
 	}
 
-	{
-		static struct cmp{ bool opCall(int a, int b) { return a < b; }}
-		auto q = PriorityQueue!(int, cmp)(cmp.init, [7,9,2,3,4,1,6,5,8,0]);
-		int i = 0;
-		while(!q.empty)
-			assert(q.pop == i++);
-	}
+	Compare cmp;
+	auto q = PriorityQueue!(int, Compare)(cmp);
+	q.push([7,9,2,3,4,1,6,5,8,0]);
+	int i = 0;
+	while(!q.empty)
+		assert(q.pop == i++);
 }
