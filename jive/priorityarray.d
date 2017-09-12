0c7597d67f06084dcfc93b8e8932d60ed0b2cc36
Simon Bürger <simon.buerger@rwth-aachen.de>
2017-09-12 13:45:46 +0200

    custom predicates for PriorityArray

--- jive/priorityarray.d
+++ jive/priorityarray.d
@@ -5,9 +5,11 @@ Authors: Simon Bürger
 
 module jive.priorityarray;
 
-private import std.algorithm;
-private import std.range;
-private import jive.array;
+import std.algorithm;
+import std.range;
+import std.functional;
+import jive.array;
+import jive.internal;
 
 /**
  * Array-like structure with fast access to the smallest element.
@@ -15,36 +17,69 @@ private import jive.array;
  * Implemented using a segment tree.
  * TODO: more array-like operations (e.g. pushBack, popBack, maybe iteration)
  */
-struct PriorityArray(V)
+struct PriorityArray(V, alias _pred = "a < b")
 {
 	// NOTE: seg[0] is unused, all unused are set to -1. length of seg is always an even power of 2
 	private Array!int seg;
-
 	private Array!V data;
 
-	/** constructor for given size */
-	this(size_t size)
-	{
-		data.resize(size);
+	mixin PredicateHelper!(_pred, V);
 
-		size_t sizeRounded = 2;
-		while(sizeRounded < size)
-			sizeRounded *= 2;
-		seg.resize(sizeRounded, -1);
-		for(int i = 0; i < length/2; ++i)
-			seg[seg.length/2 + i] = i*2;
-		for(int i = cast(int)seg.length/2-1; i >= 0; --i)
-			seg[i] = seg[2*i];
+	static if(dynamicPred)
+	{
+		/** constructor for given size */
+		this(_pred p, size_t size)
+		{
+			this.pred = p;
+
+			data.resize(size);
+
+			size_t sizeRounded = 2;
+			while(sizeRounded < size)
+				sizeRounded *= 2;
+			seg.resize(sizeRounded, -1);
+			for(int i = 0; i < length/2; ++i)
+				seg[seg.length/2 + i] = i*2;
+			for(int i = cast(int)seg.length/2-1; i > 0; --i)
+				seg[i] = seg[2*i];
+		}
+
+		/** constructor taking a range */
+		this(Stuff)(_pred p, Stuff data)
+			if(isInputRange!Stuff && is(ElementType!Stuff:V) && hasLength!Stuff)
+		{
+			this(p, data.length);
+			size_t i = 0;
+			foreach(x; data)
+				this[i++] = x;
+		}
 	}
-
-	/** constructor taking a range */
-	this(Stuff)(Stuff data)
-		if(isInputRange!Stuff && is(ElementType!Stuff:V) && hasLength!Stuff)
+	else
 	{
-		this(data.length);
-		size_t i = 0;
-		foreach(x; data)
-			this[i++] = x;
+		/** constructor for given size */
+		this(size_t size)
+		{
+			data.resize(size);
+
+			size_t sizeRounded = 2;
+			while(sizeRounded < size)
+				sizeRounded *= 2;
+			seg.resize(sizeRounded, -1);
+			for(int i = 0; i < length/2; ++i)
+				seg[seg.length/2 + i] = i*2;
+			for(int i = cast(int)seg.length/2-1; i > 0; --i)
+				seg[i] = seg[2*i];
+		}
+
+		/** constructor taking a range */
+		this(Stuff)(Stuff data)
+			if(isInputRange!Stuff && is(ElementType!Stuff:V) && hasLength!Stuff)
+		{
+			this(data.length);
+			size_t i = 0;
+			foreach(x; data)
+				this[i++] = x;
+		}
 	}
 
 	/** number of elements in the array */
@@ -79,13 +114,13 @@ struct PriorityArray(V)
 
 		int k = cast(int)i/2 + cast(int)seg.length/2;
 
-		if((i^1) < length && data[i^1] < data[i])
+		if((i^1) < length && pred(data[i^1], data[i]))
 			seg[k] = i^1;
 		else
 			seg[k] = i;
 
 		for(; k != 1; k /= 2)
-			if(seg[k^1] != -1 && data[seg[k^1]] < data[seg[k]])
+			if(seg[k^1] != -1 && pred(data[seg[k^1]], data[seg[k]]))
 				seg[k/2] = seg[k^1];
 			else
 				seg[k/2] = seg[k];
@@ -122,4 +157,35 @@ struct PriorityArray(V)
 
 	assert(a.minIndex == 2);
 	assert(a.min == -3);
+}
+
+
+/// custom predicate
+unittest
+{
+	struct Cmp
+	{
+		bool opCall(int a, int b)
+		{
+			return a > b;
+		}
+	}
+
+	// The function Cmp.opCall is not static. Therefore the constructor of
+	// PriorityArray takes an instance of Cmp
+	auto a = PriorityArray!(int, Cmp)(Cmp.init, [-7,-9,-2,-3,-4,-1,-6,-5,-8,-0]);
+	assert(a[] == [-7,-9,-2,-3,-4,-1,-6,-5,-8,-0]);
+
+	assert(a.minIndex == 9);
+	assert(a.min == -0);
+
+	a[9] = -100;
+
+	assert(a.minIndex == 5);
+	assert(a.min == -1);
+
+	a[2] = 3;
+
+	assert(a.minIndex == 2);
+	assert(a.min == 3);
 }
